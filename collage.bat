675537c17df98c4120e5a00e3cdf3d1703e40b5b
Alexander Gruschina <grusch.alex@gmail.com>
2017-04-16 17:33:13 +0200

    Added .bat file for Windows

---
+++ collage.bat
@@ -0,0 +1,7 @@
+set SRC=input/
+del *.png
+
+for %%F in (%SRC%*.jpg) do magick convert %SRC%%%F -gamma .45455 -resize "256x256^^" -crop 256x256+0+0 +repage -gamma 2.2 !RANDOM!_%%~nF.png
+  
+magick montage *.png -geometry 256x256+0+0 -tile 10x10 collage.jpg
+del *.png
