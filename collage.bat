72575f2c67af538a371b517a90b64b8357154309
Alexander Gruschina <grusch.alex@gmail.com>
2017-04-19 19:50:47 +0200

    fix creating random number

--- collage.bat
+++ collage.bat
@@ -2,7 +2,7 @@ REM based on the forum topic found here: http://www.imagemagick.org/discourse-se
 set SRC=input/
 del *.png
 
-for %%F in (%SRC%*.jpg) do magick convert %SRC%%%F -gamma .45455 -resize "256x256^^" -crop 256x256+0+0 +repage -gamma 2.2 !RANDOM!_%%~nF.png
+for %%F in (%SRC%*.jpg) do magick convert %SRC%%%F -gamma .45455 -resize "256x256^^" -crop 256x256+0+0 +repage -gamma 2.2 %RANDOM%_%%~nF.png
   
 magick montage *.png -geometry 256x256+0+0 -tile 10x10 collage.jpg
 del *.png
