16098a1ea1074f1fd4192b938bc4549d97c20c57
Alexander Gruschina <grusch.alex@gmail.com>
2017-04-19 20:00:33 +0200

    fix random number usage
    before: loop-statements were evaluated before execution (so only 1 random number was used for all file names)
    now: evaluate statements at runtime

--- collage.bat
+++ collage.bat
@@ -1,8 +1,10 @@
-REM based on the forum topic found here: http://www.imagemagick.org/discourse-server/viewtopic.php?t=16249
+REM based on the blog-question found here: http://www.imagemagick.org/discourse-server/viewtopic.php?t=16249
 set SRC=input/
 del *.png
 
-for %%F in (%SRC%*.jpg) do magick convert %SRC%%%F -gamma .45455 -resize "256x256^^" -crop 256x256+0+0 +repage -gamma 2.2 %RANDOM%_%%~nF.png
+setlocal EnableDelayedExpansion
+
+for %%F in (%SRC%*.jpg) do magick convert %SRC%%%F -gamma .45455 -resize "256x256^^" -crop 256x256+0+0 +repage -gamma 2.2 !RANDOM!_%%~nF.png
   
-magick montage *.png -geometry 256x256+0+0 -tile 10x10 collage.jpg
+magick montage *.png -geometry 256x256+0+0 -tile 10x15 collage.jpg
 del *.png
